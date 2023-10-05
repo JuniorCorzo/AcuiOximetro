224dd143a73baa2c0fccdf3a248ce66d2966c5c0
JuniorCorzo <josedanielmmf@gmail.com>
2023-10-04 19:44:01 -0500

    Creado el repositorio de usuario

---
+++ src/main/java/com/empresa/dam/apiacuioximetro/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.empresa.dam.apiacuioximetro.repository;
+
+import com.empresa.dam.apiacuioximetro.entity.Usuario;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface UsuarioRepository extends ListCrudRepository<Usuario, Integer> {
+}
