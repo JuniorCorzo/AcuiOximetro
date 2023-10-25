8d73f0bd17151f187801ae951429d0a9bb5dd5be
JuniorCorzo <josedanielmmf@gmail.com>
2023-10-24 23:05:43 -0500

    Se añadio logica de negocio para el servicio de especie

--- src/main/java/com/empresa/dam/apiacuioximetro/repository/EspecieRepository.java
+++ src/main/java/com/empresa/dam/apiacuioximetro/repository/EspecieRepository.java
@@ -2,5 +2,7 @@ package com.empresa.dam.apiacuioximetro.repository;
 
 import com.empresa.dam.apiacuioximetro.entity.Especie;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EspecieRepository extends ListCrudRepository<Especie, Integer> {}
--- src/main/java/com/empresa/dam/apiacuioximetro/service/EspecieServiceCrud.java
+++ src/main/java/com/empresa/dam/apiacuioximetro/service/EspecieServiceCrud.java
@@ -1,4 +1,31 @@
 package com.empresa.dam.apiacuioximetro.service;
 
+import com.empresa.dam.apiacuioximetro.entity.Especie;
+import com.empresa.dam.apiacuioximetro.repository.EspecieRepository;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@NoArgsConstructor
 public class EspecieServiceCrud {
+
+    private EspecieRepository repository;
+
+    @Autowired
+    public EspecieServiceCrud(EspecieRepository repository) {
+        this.repository = repository;
+    }
+
+    public Especie findById(int id){
+        return repository.findById(id).orElseThrow();
+    }
+    
+    public void create(Especie especie){
+        repository.save(especie);
+    }
+
+    public void delete(int id){
+        repository.deleteById(id);
+    }
 }
