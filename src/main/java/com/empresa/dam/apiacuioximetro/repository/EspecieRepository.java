09bc219a2047491c114b5ee22ed3d84aaf3e6360
JuniorCorzo <josedanielmmf@gmail.com>
2023-10-24 22:43:11 -0500

    Se creo el repositorio de Especie

---
+++ src/main/java/com/empresa/dam/apiacuioximetro/repository/EspecieRepository.java
@@ -0,0 +1,6 @@
+package com.empresa.dam.apiacuioximetro.repository;
+
+import com.empresa.dam.apiacuioximetro.entity.Especie;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface EspecieRepository extends ListCrudRepository<Especie, Integer> {}
