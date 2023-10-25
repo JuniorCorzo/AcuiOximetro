f64e5a791c13071a4156d1be5eb1ae9c4d4d4253
JuniorCorzo <josedanielmmf@gmail.com>
2023-10-25 18:43:32 -0500

    Se creo el servicio de Acuicola

--- src/main/java/com/empresa/dam/apiacuioximetro/service/AcuicolaServiceCrud.java
+++ src/main/java/com/empresa/dam/apiacuioximetro/service/AcuicolaServiceCrud.java
@@ -1,2 +1,21 @@
-package com.empresa.dam.apiacuioximetro.service;public class AcuicolaServiceCrud {
+package com.empresa.dam.apiacuioximetro.service;
+
+import com.empresa.dam.apiacuioximetro.entity.Acuicula;
+import com.empresa.dam.apiacuioximetro.repository.AcuicolaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AcuicolaServiceCrud {
+
+    private final AcuicolaRepository repository;
+
+    @Autowired
+    public AcuicolaServiceCrud(AcuicolaRepository repository) {
+        this.repository = repository;
+    }
+
+    public Acuicula findById(int id){
+        return this.repository.findById(id).orElseThrow();
+    }
 }
