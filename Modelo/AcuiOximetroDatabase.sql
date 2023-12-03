300e29f30e310718b2dbe37334c116b505c4bc98
JuniorCorzo <josedanielmmf@gmail.com>
2023-12-02 22:06:36 -0500

    Se añadio backup de la estructura de la base de datos del proyecto

---
+++ Modelo/AcuiOximetroDatabase.sql
@@ -0,0 +1,119 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: acuioximetro
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `acuicola`
+--
+
+DROP TABLE IF EXISTS `acuicola`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `acuicola` (
+  `id_acuicola` varchar(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `id_usuario` int NOT NULL,
+  `id_estanque` int NOT NULL,
+  PRIMARY KEY (`id_acuicola`),
+  KEY `fk_id_acuicola_idx` (`id_acuicola`),
+  KEY `fk_acuicola_usuario1_idx` (`id_usuario`),
+  KEY `fk_acuicola_estanques1_idx` (`id_estanque`),
+  CONSTRAINT `fk_acuicola_estanques1` FOREIGN KEY (`id_estanque`) REFERENCES `estanques` (`id_especie`),
+  CONSTRAINT `fk_acuicola_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `especie`
+--
+
+DROP TABLE IF EXISTS `especie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `especie` (
+  `id_especie` int NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_especie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `estanques`
+--
+
+DROP TABLE IF EXISTS `estanques`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estanques` (
+  `id_estanques` varchar(5) NOT NULL,
+  `id_especie` int NOT NULL,
+  `tipo_estanques` varchar(255) NOT NULL,
+  `cantidad_peces` int DEFAULT '0',
+  PRIMARY KEY (`id_estanques`),
+  KEY `fk_id_estanques_idx` (`id_estanques`),
+  KEY `fk_estanques_especie1_idx` (`id_especie`),
+  CONSTRAINT `fk_estanques_especie1` FOREIGN KEY (`id_especie`) REFERENCES `especie` (`id_especie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `historia_oxigeno`
+--
+
+DROP TABLE IF EXISTS `historia_oxigeno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historia_oxigeno` (
+  `id_estanques` varchar(5) NOT NULL,
+  `Nivel_oxigenacion` double NOT NULL,
+  `fecha_hora_medicion` datetime NOT NULL,
+  KEY `fk_historia_oxigeno_estanques_idx` (`id_estanques`),
+  CONSTRAINT `fk_historia_oxigeno_estanques` FOREIGN KEY (`id_estanques`) REFERENCES `estanques` (`id_estanques`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id_usuario` int NOT NULL,
+  `rol` varchar(50) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `clave` varchar(255) NOT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `id_usuario_UNIQUE` (`id_usuario`),
+  UNIQUE KEY `correo_UNIQUE` (`correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-02 22:04:02
